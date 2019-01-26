73e73694ba5f78f4a71f9b60fbdc43ca4abecdeb
Sartharis <jan.orlowsk@gmail.com>
2019-01-26 13:40:34 -0500

    Sounds and intro level added, modified inputs as well

---
+++ GGJ 2019/Assets/FadeOutColor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOutColor : MonoBehaviour
+{
+    [SerializeField] float beforeFadeOutTime = 3f;
+    [SerializeField] float fadeOutTime = 1f;
+    private float currentFadeOutTime = 0.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentFadeOutTime = fadeOutTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(beforeFadeOutTime <= 0)
+        {
+            if (currentFadeOutTime > 0)
+            {
+                currentFadeOutTime -= Time.deltaTime;
+            }
+            else
+            {
+                currentFadeOutTime = 0;
+            }
+
+            Graphic[] images = GetComponents<Graphic>();
+            foreach (Graphic i in images)
+            {
+                i.color = new Color(i.color.r, i.color.g, i.color.b, currentFadeOutTime / fadeOutTime);
+            }
+        }
+        else
+        {
+            beforeFadeOutTime -= Time.deltaTime;
+        }
+
+    }
+}
