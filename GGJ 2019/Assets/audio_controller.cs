55f232c6c30026df4dd503f5ac7bdf22415ca097
Sartharis <jan.orlowsk@gmail.com>
2019-01-26 17:18:44 -0500

    Audio controller + meta

--- GGJ 2019/Assets/audio_controller.cs
+++ GGJ 2019/Assets/audio_controller.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class audio_controller : MonoBehaviour
 {
-    [SerializeField] private string_event_object 
+    [SerializeField] private audio_event_object audio_event;
+    [SerializeField] private AudioSource oneShotSource;
+
+    void OnAudioEvent(Vector3 position, AudioClip clip)
+    {
+        AudioSource.PlayClipAtPoint(clip, position);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio_event.addListener(OnAudioEvent);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- GGJ 2019/Assets/audio_event_object.cs
+++ GGJ 2019/Assets/audio_event_object.cs
@@ -3,11 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public struct AudioEvent
-{
-    public Vector3 location;
-    public AudioClip clip;
-}
-
 [CreateAssetMenu(menuName = "events/audio_event")]
 public class audio_event_object : gen_event<Vector3,AudioClip>{}
