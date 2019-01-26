25a73dc49be7df25d4f9758340a9ff28a58596a0
Sartharis <jan.orlowsk@gmail.com>
2019-01-26 16:22:53 -0500

    audio controller

---
+++ GGJ 2019/Assets/audio_event_object.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public struct AudioEvent
+{
+    public Vector3 location;
+    public AudioClip clip;
+}
+
+[CreateAssetMenu(menuName = "events/audio_event")]
+public class audio_event_object : ScriptableObject
+{
+    [SerializeField]
+    AudioEvent constant;
+
+    public class string_event : UnityEvent<AudioEvent> { }
+
+    public UnityEvent<AudioEvent> e = new string_event();
+
+    public void Invoke(string d) { e.Invoke(d); }
+
+    public void Invoke() { e.Invoke(constant); }
+
+}
