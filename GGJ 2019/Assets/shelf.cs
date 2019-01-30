e4483bdd5c830df11eb8ee03dd415eae12cdb88a
Sartharis <jan.orlowsk@gmail.com>
2019-01-30 18:15:56 -0500

    Shelves + glass

---
+++ GGJ 2019/Assets/shelf.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shelf : Interactable
+{
+
+    [SerializeField]
+    Vector3 offset;
+    private Vector3 startPoint;
+    [SerializeField]
+    private bool opened = false;
+
+    [SerializeField] private AudioClip openSound;
+    [SerializeField] private AudioClip closeSound;
+
+    private void Start()
+    {
+        startPoint = transform.position;
+    }
+
+    public override void act()
+    {
+        opened = !opened;
+        if (opened)
+        {
+             if(openSound) AudioSource.PlayClipAtPoint(openSound, transform.position);
+        }
+        else
+        {
+             if(closeSound) AudioSource.PlayClipAtPoint(closeSound, transform.position);
+        }
+    }
+
+    private void Update()
+    {
+        if(opened)
+        {
+            transform.position = Vector3.Lerp(transform.position, startPoint + offset, 0.1f);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, startPoint, 0.1f);
+        }
+    }
+
+
+}
+
